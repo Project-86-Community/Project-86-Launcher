71c031169092e2a387a87afc5ad0bee04c020ebf
realskyquest <135049479+realskyquest@users.noreply.github.com>
2025-05-17 01:05:45 +0500

    p86l: sidebar, fix focus

--- sidebar.go
+++ sidebar.go
@@ -104,31 +104,6 @@ func (s *sidebarContent) Build(context *guigui.Context, appender *guigui.ChildWi
 	return nil
 }
 
-func (s *sidebarContent) HandlePointingInput(context *guigui.Context) guigui.HandleInputResult {
-	_, dy := ebiten.Wheel()
-
-	currentIndex := s.list.SelectedItemIndex()
-	itemsCount := s.list.ItemsCount()
-
-	newIndex := currentIndex - int(dy)
-
-	if newIndex < 0 {
-		newIndex = 0
-	} else if newIndex >= itemsCount {
-		newIndex = itemsCount - 1
-	}
-
-	if newIndex != currentIndex {
-		s.list.SelectItemByIndex(newIndex)
-		if item, ok := s.list.ItemByIndex(newIndex); ok && item.ID != s.model.Mode() {
-			s.model.SetMode(item.ID)
-		}
-		return guigui.HandleInputByWidget(s)
-	}
-
-	return guigui.HandleInputResult{}
-}
-
 func (s *sidebarContent) HandleButtonInput(context *guigui.Context) guigui.HandleInputResult {
 	currentIndex := s.list.SelectedItemIndex()
 	itemsCount := s.list.ItemsCount()
@@ -157,4 +132,30 @@ func (s *sidebarContent) HandleButtonInput(context *guigui.Context) guigui.Handl
 	}
 
 	return guigui.HandleInputResult{}
+}
+
+func (s *sidebarContent) Tick(context *guigui.Context) error {
+	if context.IsFocusedOrHasFocusedChild(s) {
+		_, dy := ebiten.Wheel()
+
+		currentIndex := s.list.SelectedItemIndex()
+		itemsCount := s.list.ItemsCount()
+
+		newIndex := currentIndex - int(dy)
+
+		if newIndex < 0 {
+			newIndex = 0
+		} else if newIndex >= itemsCount {
+			newIndex = itemsCount - 1
+		}
+
+		if newIndex != currentIndex {
+			s.list.SelectItemByIndex(newIndex)
+			if item, ok := s.list.ItemByIndex(newIndex); ok && item.ID != s.model.Mode() {
+				s.model.SetMode(item.ID)
+			}
+		}
+	}
+
+	return nil
 }
